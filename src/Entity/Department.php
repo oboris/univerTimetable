892c293c387adedd32b715d51e4ddeb455217fd8
Boris Onishchenko <oboris@ukr.net>
2019-10-27 19:11:07 +0200

    created entities

---
+++ src/Entity/Department.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="department")
+ */
+class Department
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\staff", cascade={"persist", "remove"})
+     */
+    private $teamLead;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\staff", inversedBy="departments")
+     */
+    private $staffs;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\company", inversedBy="departments")
+     */
+    private $company;
+
+    public function __construct()
+    {
+        $this->staffs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTeamLead(): ?staff
+    {
+        return $this->teamLead;
+    }
+
+    public function setTeamLead(?staff $teamLead): self
+    {
+        $this->teamLead = $teamLead;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|staff[]
+     */
+    public function getStaffs(): Collection
+    {
+        return $this->staffs;
+    }
+
+    public function addStaff(staff $staff): self
+    {
+        if (!$this->staffs->contains($staff)) {
+            $this->staffs[] = $staff;
+        }
+
+        return $this;
+    }
+
+    public function removeStaff(staff $staff): self
+    {
+        if ($this->staffs->contains($staff)) {
+            $this->staffs->removeElement($staff);
+        }
+
+        return $this;
+    }
+
+    public function getCompany(): ?company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?company $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+}
---
+++ src/Entity/ProjectPeople.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="project_people")
+ */
+class ProjectPeople
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $responsibility;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\project", inversedBy="projectPeople")
+     */
+    private $project;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\staff", inversedBy="projectPeople")
+     */
+    private $staff;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getResponsibility(): ?string
+    {
+        return $this->responsibility;
+    }
+
+    public function setResponsibility(string $responsibility): self
+    {
+        $this->responsibility = $responsibility;
+
+        return $this;
+    }
+
+    public function getProject(): ?project
+    {
+        return $this->project;
+    }
+
+    public function setProject(?project $project): self
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    public function getStaff(): ?staff
+    {
+        return $this->staff;
+    }
+
+    public function setStaff(?staff $staff): self
+    {
+        $this->staff = $staff;
+
+        return $this;
+    }
+}
