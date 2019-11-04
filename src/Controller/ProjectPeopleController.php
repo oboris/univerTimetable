97ceaf7761ca13582208a45a85de4eb44195d1e3
Boris Onishchenko <oboris@ukr.net>
2019-11-04 16:30:37 +0200

    corrected route in ProjectPeople controller from /project/people to project_people

--- src/Controller/ProjectPeopleController.php
+++ src/Controller/ProjectPeopleController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/project/people")
+ * @Route("/project_people")
  */
 class ProjectPeopleController extends AbstractController
 {
